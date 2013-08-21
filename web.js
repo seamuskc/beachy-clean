16793791e9f850e9682cc5beaa15ad0ec56bc279
Sean Casey <ws.casey@yahoo.com>
2013-08-21 06:33:50 +0000

    start of adding mongo functionality

--- web.js
+++ web.js
@@ -4,6 +4,11 @@ app.use(express.bodyParser());
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
+var databaseUrl = "test"; // "username:password@example.com/mydb"
+var collections = ["users"]
+var mongojs = require("mongojs");
+var db = mongojs.connect(databaseUrl, collections);
+var ObjectId = mongojs.ObjectId;
 
 app.get('/register', function(request, response) {
     
@@ -11,9 +16,53 @@ app.get('/register', function(request, response) {
 
 });
 
+app.get('/users/list', function(request, response) {
+    
+    db.users.find({}, function(err, users) {
+        var output = "";
+        if( err || !users) {
+            response.send("No Users users found");
+        }
+        else {
+            response.render("userList", {title:"User List", users:users});
+            //users.forEach( function(user) {
+            //    output += JSON.stringify(user);
+            //});
+        }
+        
+        //response.send(output);
+    });
+    
+    
+});
+
 app.post('/register', function(request, response) {
     
-    response.render('confirm', {title:"Confirm", firstName:request.body.firstName, lastName:request.body.lastName});
+    var id = ObjectId();
+    db.users.save({
+        _id: id,
+        firstName:request.body.firstName,
+        lastName:request.body.lastName,
+        email:request.body.email,
+        password:request.body.password
+    });
+    
+    response.render('confirm', {
+        title:"Confirm",
+        firstName:request.body.firstName,
+        lastName:request.body.lastName,
+        email:request.body.email
+    });
+
+});
+
+app.get('/users/delete/:id', function(request, response) {
+    
+    var id = ObjectId(request.params.id);
+    console.log('Removing user with id:' + id);
+    console.log(db.users.remove({_id: id}));
+    
+    response.send("Deleted user " + id);
 
 });
 
